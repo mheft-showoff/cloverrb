6666eb7b40387a5ebd6374dbfcb3c706f3921825
Michael Heft <mheft@showoff.ie>
2019-11-04 09:25:48 +0000

    Refactor modifier groups url

--- lib/cloverrb/modifier_group.rb
+++ lib/cloverrb/modifier_group.rb
@@ -2,13 +2,13 @@ module Cloverrb
   class ModifierGroup < Client
 
     def all(role=nil)
-      url = "/merchants/#{@merchant_id}/tags"
+      url = "/merchants/#{@merchant_id}/modifier_groups"
       url += "?filter=role=#{role}" if role
       get(@token, url)
     end
 
     def find(id)
-      url = "/merchants/#{@merchant_id}/tags/#{id}"
+      url = "/merchants/#{@merchant_id}/modifier_groups/#{id}"
       get(@token, url)
     end
   end
